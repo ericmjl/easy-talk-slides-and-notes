6b64f1b2355731a070ef94781cee30e84f3bcd3b
Eric J. Ma <ericmajinglong@gmail.com>
2016-12-15 15:29:52 -0500

    removed gh_init after usage

--- gh_init.sh
+++
@@ -1,5 +0,0 @@
-# This script will run the necessary git commands to point to GitHub 
-
-git remote add origin git@github.com:ericmjl/easy-talk-slides-and-notes
-git push -u origin master
-rm initialize_upload.sh
